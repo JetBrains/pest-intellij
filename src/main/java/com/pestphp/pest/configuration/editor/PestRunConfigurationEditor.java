c434e4001473b029232c420007842443bb105348
Oliver Nybroe <olivernybroe@gmail.com>
2020-06-17 20:03:26 +0200

    feat(add-php-command-line-config): Add the PhpCommandLineConfigurationEditor panel, by using reflections

--- src/main/java/com/pestphp/pest/configuration/editor/PestRunConfigurationEditor.java
+++ src/main/java/com/pestphp/pest/configuration/editor/PestRunConfigurationEditor.java
@@ -1,15 +1,17 @@
 package com.pestphp.pest.configuration.editor;
 
-import com.intellij.openapi.actionSystem.impl.ActionButton;
 import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.options.SettingsEditor;
 import com.intellij.openapi.project.Project;
 import com.jetbrains.php.run.PhpCommandLineConfigurationEditor;
-import com.jetbrains.php.run.PhpRunConfigurationInterpreterCombobox;
 import com.pestphp.pest.configuration.PestRunConfiguration;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import java.awt.*;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class PestRunConfigurationEditor extends SettingsEditor<PestRunConfiguration> {
     @NotNull private final Project project;
@@ -19,15 +21,13 @@ public class PestRunConfigurationEditor extends SettingsEditor<PestRunConfigurat
     private JPanel pestSettingsPanel;
     private PestSettingsEditor pestSettingsEditor;
 
-    private JPanel cliSettingsPanel;
     private PhpCommandLineConfigurationEditor phpCommandLineConfigurationEditor;
 
     public PestRunConfigurationEditor(@NotNull final Project project) {
         super();
         this.project = project;
-        phpCommandLineConfigurationEditor = new PhpCommandLineConfigurationEditor();
 
-        phpCommandLineConfigurationEditor.init(project, true);
+        createUIComponents();
     }
 
     @Override
@@ -49,6 +49,21 @@ public class PestRunConfigurationEditor extends SettingsEditor<PestRunConfigurat
     }
 
     private void createUIComponents() {
-        pestSettingsEditor = new PestSettingsEditor(project);
+        panel = new JPanel();
+        /*panel.setBorder(BorderFactory.createTitledBorder(
+            BorderFactory.createMatteBorder(1, 0, 0, 0, JBColor.LIGHT_GRAY),
+            "some title"
+        );*/
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        try {
+            phpCommandLineConfigurationEditor = new PhpCommandLineConfigurationEditor();
+            phpCommandLineConfigurationEditor.init(project);
+            Method method = phpCommandLineConfigurationEditor.getClass().getMethod("$$$getRootComponent$$$");
+
+            panel.add((Component) method.invoke(phpCommandLineConfigurationEditor));
+        }  catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
     }
 }
