10ab98938b073bcc1659e48fd2c99ba49fb750e8
Oliver Nybroe <olivernybroe@gmail.com>
2020-07-09 19:56:23 +0200

    feat(debugger): Add support for running as debugger

--- src/main/kotlin/com/pestphp/pest/configuration/PestDebugRunner.kt
+++ src/main/kotlin/com/pestphp/pest/configuration/PestDebugRunner.kt
@@ -1,24 +1,9 @@
-package com.pestphp.pest.configuration;
+package com.pestphp.pest.configuration
 
-import com.intellij.execution.ExecutionException;
-import com.intellij.execution.configurations.RunProfileState;
-import com.intellij.execution.runners.ExecutionEnvironment;
-import com.intellij.execution.ui.RunContentDescriptor;
-import com.jetbrains.php.testFramework.run.PhpTestDebugRunner;
-import org.jetbrains.annotations.NotNull;
+import com.jetbrains.php.testFramework.run.PhpTestDebugRunner
 
-public class PestDebugRunner extends PhpTestDebugRunner<PestRunConfiguration> {
-    protected PestDebugRunner() {
-        super(PestRunConfiguration.class);
-    }
-
-    @Override
-    protected RunContentDescriptor doExecute(@NotNull PestRunConfiguration pestRunConfiguration, @NotNull RunProfileState runProfileState, @NotNull ExecutionEnvironment executionEnvironment) throws ExecutionException {
-        return null;
-    }
-
-    @Override
-    public @NotNull String getRunnerId() {
-        return "PestDebugRunner";
+class PestDebugRunner private constructor() : PhpTestDebugRunner<PestRunConfiguration>(PestRunConfiguration::class.java) {
+    override fun getRunnerId(): String {
+        return "PestDebugRunner"
     }
 }
