116e0e2347c9481708b5ea776252bcb77faea26b
Oliver Nybroe <olivernybroe@gmail.com>
2020-07-09 20:40:18 +0200

    test(icon provider): Add test for file icon

---
+++ src/test/kotlin/com/pestphp/pest/tests/PestIconProviderTest.kt
@@ -0,0 +1,37 @@
+package com.pestphp.pest.tests
+
+import com.intellij.openapi.util.Iconable.ICON_FLAG_VISIBILITY
+import com.pestphp.pest.PestIconProvider
+import com.pestphp.pest.PestIcons
+import junit.framework.TestCase
+import kotlin.test.assertNotEquals
+
+class PestIconProviderTest: PestLightCodeFixture() {
+    override fun setUp() {
+        super.setUp()
+
+        myFixture.copyFileToProject("SimpleTest.php")
+    }
+
+    override fun getTestDataPath(): String? {
+        return basePath + "fixtures"
+    }
+
+    fun testCanGetPestIconForPestFile() {
+        val file = myFixture.configureByFile("SimpleTest.php")
+
+        TestCase.assertEquals(
+            PestIconProvider().getIcon(file, ICON_FLAG_VISIBILITY),
+            PestIcons.FILE
+        )
+    }
+
+    fun testCanGetOtherIconForNonPestFile() {
+        val file = myFixture.configureByFile("SimpleScript.php")
+
+        assertNotEquals(
+            PestIconProvider().getIcon(file, ICON_FLAG_VISIBILITY),
+            PestIcons.FILE
+        )
+    }
+}
