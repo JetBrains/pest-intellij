59f70341c52e20d019b0698ae10a835663e11539
Oliver Nybroe <olivernybroe@gmail.com>
2020-09-06 08:42:50 +0200

    fix(runner): Fix finding tests in namespace

--- src/test/kotlin/com/pestphp/pest/tests/types/BaseTypeTest.kt
+++ src/test/kotlin/com/pestphp/pest/tests/types/BaseTypeTest.kt
@@ -2,7 +2,7 @@ package com.pestphp.pest.tests.types
 
 import com.pestphp.pest.tests.PestLightCodeFixture
 
-abstract class BaseTypeTest: PestLightCodeFixture() {
+abstract class BaseTypeTest : PestLightCodeFixture() {
     override fun setUp() {
         super.setUp()
 
