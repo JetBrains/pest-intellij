763834ee9646c19a907746397fe1203657cbfb71
Oliver Nybroe <oliver@worksome.com>
2021-03-09 07:48:39 +0100

    fix(error): Fix exception from pest test file

--- src/main/kotlin/com/pestphp/pest/PestIconProvider.kt
+++ src/main/kotlin/com/pestphp/pest/PestIconProvider.kt
@@ -1,16 +1,19 @@
 package com.pestphp.pest
 
 import com.intellij.ide.IconProvider
+import com.intellij.openapi.project.DumbService
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import javax.swing.Icon
 
 class PestIconProvider : IconProvider() {
     override fun getIcon(element: PsiElement, flags: Int): Icon? {
-        if (element is PsiFile && element.isPestTestFile()) {
-            return PestIcons.FILE
-        }
+        if (DumbService.isDumb(element.project)) return null
 
-        return null
+        if (element !is PsiFile) return null
+
+        if (!element.isPestTestFile()) return null
+
+        return PestIcons.FILE
     }
 }
