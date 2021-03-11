8ac8568b8cb0cce923b655b2d3eb56c79870aab4
Oliver Nybroe <oliver@worksome.com>
2021-03-11 09:04:15 +0100

    Update gradle build file with template

--- src/main/kotlin/com/pestphp/pest/notifications/OutdatedNotification.kt
+++ src/main/kotlin/com/pestphp/pest/notifications/OutdatedNotification.kt
@@ -1,17 +1,13 @@
 package com.pestphp.pest.notifications
 
 import com.intellij.notification.Notification
-import com.intellij.notification.NotificationDisplayType
-import com.intellij.notification.NotificationGroup
+import com.intellij.notification.NotificationGroupManager
 import com.intellij.notification.NotificationType
 import com.intellij.openapi.project.Project
 
 class OutdatedNotification {
-    private val group = NotificationGroup(
-        "Outdated Pest",
-        NotificationDisplayType.BALLOON,
-        true
-    )
+    private val group = NotificationGroupManager.getInstance()
+        .getNotificationGroup("Outdated Pest")
 
     fun notify(content: String): Notification {
         return notify(null, content)
