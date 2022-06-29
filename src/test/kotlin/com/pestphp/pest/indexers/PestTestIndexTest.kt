15903e803aa3b4daad4afccd89bc4a885f1eab9f
Oliver <oliver@worksome.com>
2022-06-29 07:31:47 +0200

    Fix broken tests

--- src/test/kotlin/com/pestphp/pest/indexers/PestTestIndexTest.kt
+++ src/test/kotlin/com/pestphp/pest/indexers/PestTestIndexTest.kt
@@ -1,16 +1,20 @@
 package com.pestphp.pest.indexers
 
 import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.testFramework.TestDataPath
 import com.intellij.util.indexing.FileBasedIndex
 import com.pestphp.pest.PestLightCodeFixture
 
+@TestDataPath("\$CONTENT_ROOT/resources/com/pestphp/pest/indexers/PestTestIndexTest")
 class PestTestIndexTest : PestLightCodeFixture() {
     override fun getTestDataPath(): String {
         return "src/test/resources/com/pestphp/pest/indexers/PestTestIndexTest"
     }
 
     fun testPestTestFileIsIndexed() {
-        myFixture.copyFileToProject("FileWithPestTest.php", "tests/FileWithPestTest.php")
+        val virtualFile = myFixture.copyFileToProject("FileWithPestTest.php", "tests/FileWithPestTest.php")
+        myFixture.configureFromExistingVirtualFile(virtualFile)
+
 
         val fileBasedIndex = FileBasedIndex.getInstance()
 
@@ -18,7 +22,7 @@ class PestTestIndexTest : PestLightCodeFixture() {
             fileBasedIndex.getContainingFiles(PestTestIndex.key, it, GlobalSearchScope.allScope(project)).isNotEmpty()
         }
 
-        assertContainsElements(indexKeys, "FileWithPestTest.php")
+        assertContainsElements(indexKeys, "/src/tests/FileWithPestTest.php")
     }
 
     fun testPhpFileIsNotIndexed() {
