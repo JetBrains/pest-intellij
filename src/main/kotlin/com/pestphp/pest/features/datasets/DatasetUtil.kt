ec1f6408e55d5a4367ae30c83a9ce43249aa7cb6
adelf <adel.faiz@gmail.com>
2022-10-15 16:57:47 +0400

    dataset Find Usages GoTo handler

--- src/main/kotlin/com/pestphp/pest/features/datasets/DatasetUtil.kt
+++ src/main/kotlin/com/pestphp/pest/features/datasets/DatasetUtil.kt
@@ -4,9 +4,11 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.psi.search.ProjectScope
 import com.intellij.util.indexing.FileBasedIndex
+import com.jetbrains.php.lang.psi.elements.MethodReference
 import com.jetbrains.php.lang.psi.elements.StringLiteralExpression
 import com.jetbrains.php.lang.psi.elements.impl.FunctionReferenceImpl
 import com.pestphp.pest.getRootPhpPsiElements
+import com.pestphp.pest.isPestTestReference
 import com.pestphp.pest.realPath
 
 fun PsiFile.isPestDatasetFile(): Boolean {
@@ -24,7 +26,6 @@ fun PsiFile.isIndexedPestDatasetFile(): Boolean {
 
 fun PsiElement?.isPestDataset(): Boolean {
     return when (this) {
-        null -> false
         is FunctionReferenceImpl -> this.isPestDatasetFunction()
         else -> false
     }
@@ -36,6 +37,12 @@ fun FunctionReferenceImpl.isPestDatasetFunction(): Boolean {
 
 fun FunctionReferenceImpl.getPestDatasetName(): String? {
     return (getParameter(0) as? StringLiteralExpression)?.contents
+}
+
+fun MethodReference.isDatasetCall() : Boolean {
+    if (name != "with") return false
+
+    return isPestTestReference()
 }
 
 fun PsiFile.getDatasets(): List<FunctionReferenceImpl> {
---
+++ src/test/resources/com/pestphp/pest/goto/datasetUsages/DatasetUsage.php
@@ -0,0 +1,5 @@
+<?php
+
+it('can check if in the valley', function (string $dojo) {
+
+})->with([])->with('datasetName');
