803fba00fe4d4523fac54651dff7cf798032230c
Artemy Pestretsov <artemy.pestretsov@jetbrains.com>
2023-11-30 15:52:34 +0100

    [php] Pest performance: don't recompute file icon too often
    GitOrigin-RevId: 5c4dc29dd8fbd5fffcb3e8bb314b014247cf26d9

--- src/main/kotlin/com/pestphp/pest/PestIconProvider.kt
+++ src/main/kotlin/com/pestphp/pest/PestIconProvider.kt
@@ -1,34 +1,27 @@
 package com.pestphp.pest
 
-import com.intellij.ide.IconProvider
+import com.intellij.ide.FileIconProvider
 import com.intellij.openapi.project.DumbService
-import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiFile
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiManager
+import com.jetbrains.php.lang.psi.PhpFile
 import com.pestphp.pest.features.datasets.isIndexedPestDatasetFile
 import javax.swing.Icon
 
-class PestIconProvider : IconProvider() {
-    override fun getIcon(element: PsiElement, flags: Int): Icon? {
-        if (DumbService.isDumb(element.project)) return null
-
-        if (element !is PsiFile) return null
-
-        return findIconFromPsiFile(element)
-    }
-
-    private fun findIconFromPsiFile(file: PsiFile): Icon? {
+class PestIconProvider : FileIconProvider {
+    override fun getIcon(vFile: VirtualFile, flags: Int, project: Project?): Icon? {
+        if (project == null || DumbService.isDumb(project)) return null
+        val file = PsiManager.getInstance(project).findFile(vFile) as? PhpFile ?: return null
         if (file.isIndexedPestTestFile()) {
             return PestIcons.File
         }
-
         if (file.isIndexedPestDatasetFile()) {
-            return PestIcons.Dataset
+           return PestIcons.Dataset
         }
-
         if (file.isPestFile()) {
             return PestIcons.Logo
         }
-
         return null
     }
 }
