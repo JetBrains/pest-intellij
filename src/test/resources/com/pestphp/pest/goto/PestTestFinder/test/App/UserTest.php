ec8c5155569cf5697b8c76b5ce4652e99fa96dd7
Ilia Smirnov <ilia.smirnov@jetbrains.com>
2024-01-16 18:14:21 +0100

    WI-75699 Add Go To "method to test" and "test to method" navigations
    GitOrigin-RevId: 2e0dc1e8cedf3cb2dfe8505cac1201211fe76d7a

--- src/test/resources/com/pestphp/pest/goto/PestTestFinder/test/App/UserTest.php
+++ src/test/resources/com/pestphp/pest/goto/PestTestFinder/test/App/UserTest.php
@@ -5,5 +5,23 @@ use App\User;
 test("Can get user's name", function () {
    $user = new User();
 
-   $this->asserEquals("Oliver Nybroe", $user->getName());
+   $this->assertEquals("Oliver Nybroe", $user->getName());
+});
+
+test("is pest developer", function () {
+   $user = new User();
+
+   $this->assertTrue($user->isPestDeveloper());
+});
+
+test("is pest developer check if not false", function () {
+   $user = new User();
+
+   $this->assertNotEquals(false, $user->isPestDeveloper());
+});
+
+test("incorrect is pest developer", function () {
+   $user = new User();
+
+   $this->assertNotEquals(false, $user->isPestDeveloper());
 });
