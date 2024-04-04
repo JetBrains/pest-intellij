e582e99ff7341d09a11e701e3bb484c0e3631638
Andrei Efanov <andrei.efanov@jetbrains.com>
2024-04-04 17:16:05 +0200

    refactor: use VirtualFileUtil#originalFileOrSelf instead of complex typecasting with checks
    GitOrigin-RevId: 18216a313445781188461bf84935473f74b76d0b

--- src/main/kotlin/com/pestphp/pest/FileUtil.kt
+++ src/main/kotlin/com/pestphp/pest/FileUtil.kt
@@ -1,16 +1,12 @@
 package com.pestphp.pest
 
+import com.intellij.openapi.vfs.originalFileOrSelf
 import com.intellij.psi.PsiFile
-import com.intellij.testFramework.LightVirtualFile
 
 /**
  * Takes care of getting the path of a file even if it's a light file.
  */
 val PsiFile.realPath: String
     get() {
-        var virtualFile = this.viewProvider.virtualFile
-        if (virtualFile is LightVirtualFile && virtualFile.originalFile != null) {
-            virtualFile = virtualFile.originalFile
-        }
-        return virtualFile.path
+        return viewProvider.virtualFile.originalFileOrSelf().path
     }
