eb338ff5d8a53b61e066559bce98c792a0e08c83
Andrei Efanov <andrei.efanov@jetbrains.com>
2024-04-05 09:46:27 +0200

    rollback: VirtualFileUtil#originalFileOrSelf in in com.pestphp.pest.FileUtilKt.getRealPath
    GitOrigin-RevId: 8be507c8a4917b648666af6199c958aa42ea1469

--- src/main/kotlin/com/pestphp/pest/FileUtil.kt
+++ src/main/kotlin/com/pestphp/pest/FileUtil.kt
@@ -1,12 +1,16 @@
 package com.pestphp.pest
 
-import com.intellij.openapi.vfs.originalFileOrSelf
 import com.intellij.psi.PsiFile
+import com.intellij.testFramework.LightVirtualFile
 
 /**
  * Takes care of getting the path of a file even if it's a light file.
  */
 val PsiFile.realPath: String
     get() {
-        return viewProvider.virtualFile.originalFileOrSelf().path
+        var virtualFile = this.viewProvider.virtualFile
+        if (virtualFile is LightVirtualFile && virtualFile.originalFile != null) {
+            virtualFile = virtualFile.originalFile
+        }
+        return virtualFile.path
     }
