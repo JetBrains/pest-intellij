cdadcafdf84e2fff0df0e923969ff0fc09fe5df1
Ilia Smirnov <ilia.smirnov@jetbrains.com>
2024-05-06 11:34:58 +0200

    [pest] Fix PestNewTestFromClassActionTest for all tests execution by avoiding base directory index check
    GitOrigin-RevId: 63487ca3d02a7842db43a0c97e4436fb5de924cb

--- src/test/kotlin/com/pestphp/pest/generateTest/PestNewTestFromClassActionTest.kt
+++ src/test/kotlin/com/pestphp/pest/generateTest/PestNewTestFromClassActionTest.kt
@@ -1,12 +1,18 @@
 package com.pestphp.pest.generateTest
 
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.text.StringUtil
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.ex.temp.TempFileSystem
 import com.intellij.psi.PsiFile
+import com.intellij.testFramework.PlatformTestUtil
 import com.intellij.testFramework.utils.vfs.getPsiFile
+import com.intellij.util.PathUtil
 import com.jetbrains.php.lang.psi.PhpFile
 import com.jetbrains.php.lang.psi.PhpPsiUtil
 import com.jetbrains.php.lang.psi.elements.Method
 import com.jetbrains.php.phpunit.codeGeneration.PhpNewTestDialog
+import com.jetbrains.php.roots.ui.PhpPsrDirectoryComboBox
 import com.jetbrains.php.templates.PhpCreateFileFromTemplateDataProvider
 import com.pestphp.pest.PestLightCodeFixture
 import com.pestphp.pest.PestNewTestFromClassAction
@@ -21,7 +27,33 @@ class PestNewTestFromClassActionTest : PestLightCodeFixture() {
         val selectedElement = PhpPsiUtil.findClass(file as PhpFile) { _ -> true }
         val testFile = MockPestNewTestFromClassAction.publicCreateFile(
             project,
-            object : PhpNewTestDialog(project, null, file, PestTestCreateInfo, selectedElement) {
+            object : PhpNewTestDialog(project, file.containingDirectory, file, PestTestCreateInfo, selectedElement) {
+                override fun createUIComponents() {
+                    super.createUIComponents()
+                    myDirectoryCombobox = object : PhpPsrDirectoryComboBox(project, testDirectoryProvider) {
+                        override fun init(baseDir: VirtualFile, namespace: String) {
+                            super.init(baseDir, namespace)
+                            updateSuggestions(getNamespace())
+                        }
+
+                        override fun getExistingParent(): VirtualFile {
+                            return findExistingParent(selectedPath) ?: PlatformTestUtil.getOrCreateProjectBaseDir(project)
+                        }
+
+                        private fun findExistingParent(path: String): VirtualFile? {
+                            if (StringUtil.isEmpty(path)) {
+                                return null
+                            }
+                            val directory = TempFileSystem.getInstance().findFileByPath(path)
+                            return directory ?: findExistingParent(PathUtil.getParentPath(path))
+                        }
+
+                        override fun getBaseDirectory(): VirtualFile {
+                            return myDirectoryCombobox.existingParent ?: super.getBaseDirectory()
+                        }
+                    }
+                }
+
                 override fun getNamespace(): String {
                     return namespace
                 }
