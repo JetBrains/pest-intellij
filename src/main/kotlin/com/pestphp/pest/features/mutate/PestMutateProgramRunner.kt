6fc6d9a08af1798aa5ea076ceee7c27a741ec951
Ilia Smirnov <ilia.smirnov@jetbrains.com>
2024-10-01 12:32:44 +0200

    WI-79176 Collect statistics about Pest mutation testing
    GitOrigin-RevId: d3fbf8204795935f30e23bc5643089cb42187985

--- src/main/kotlin/com/pestphp/pest/features/mutate/PestMutateProgramRunner.kt
+++ src/main/kotlin/com/pestphp/pest/features/mutate/PestMutateProgramRunner.kt
@@ -1,14 +1,23 @@
 package com.pestphp.pest.features.mutate
 
 import com.intellij.execution.configurations.RunProfile
+import com.intellij.execution.configurations.RunProfileState
+import com.intellij.execution.runners.ExecutionEnvironment
+import com.intellij.execution.ui.RunContentDescriptor
 import com.pestphp.pest.configuration.PestRunConfiguration
 import com.pestphp.pest.coverage.PestCoverageProgramRunner
+import com.pestphp.pest.statistics.PestUsagesCollector
 
 private val MUTATE_ARGUMENTS: MutableList<String> = mutableListOf("--mutate")
 
 class PestMutateProgramRunner : PestCoverageProgramRunner() {
     companion object {
         const val RUNNER_ID: String = "PestMutateRunner"
+    }
+
+    override fun doExecute(state: RunProfileState, environment: ExecutionEnvironment): RunContentDescriptor? {
+        PestUsagesCollector.logMutationTestExecution(environment.project)
+        return super.doExecute(state, environment)
     }
 
     override fun canRun(executorId: String, profile: RunProfile): Boolean {
---
+++ src/main/kotlin/com/pestphp/pest/statistics/PestUsagesCollector.kt
@@ -0,0 +1,14 @@
+package com.pestphp.pest.statistics
+
+import com.intellij.internal.statistic.eventLog.EventLogGroup
+import com.intellij.internal.statistic.service.fus.collectors.CounterUsagesCollector
+import com.intellij.openapi.project.Project
+
+object PestUsagesCollector : CounterUsagesCollector() {
+    private val GROUP = EventLogGroup("pest", 1)
+    private val PEST_MUTATION_TEST_EXECUTED = GROUP.registerEvent("pest.mutation.test.executed")
+
+    fun logMutationTestExecution(project: Project) {
+        PEST_MUTATION_TEST_EXECUTED.log(project)
+    }
+}
