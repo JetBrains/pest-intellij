701f1399bfa527007a5c0498b5a562155fdd40ae
Ilia Smirnov <ilia.smirnov@jetbrains.com>
2024-10-18 18:32:43 +0200

    WI-79434 Allow running mutation and parallel tests for every Pest version, but show an error notification if it isn't possible for the current Pest version
    Mutation testing is supported from v3.0, Parallel testing in PhpStorm requires at least v2.0
    
    
    (cherry picked from commit e5acfe5052282e258cd95727eecffee246b498e4)
    
    IJ-CR-147508
    
    GitOrigin-RevId: da443a240712e6130bac08d9205a81337826e18e

--- src/main/kotlin/com/pestphp/pest/features/mutate/PestMutateProgramRunner.kt
+++ src/main/kotlin/com/pestphp/pest/features/mutate/PestMutateProgramRunner.kt
@@ -4,44 +4,32 @@ import com.intellij.execution.configurations.RunProfile
 import com.intellij.execution.configurations.RunProfileState
 import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.execution.ui.RunContentDescriptor
-import com.jetbrains.php.testFramework.PhpTestFrameworkSettingsManager
-import com.jetbrains.php.testFramework.PhpTestFrameworkVersionCache
-import com.pestphp.pest.PestFrameworkType
+import com.pestphp.pest.PestBundle
 import com.pestphp.pest.configuration.PestRunConfiguration
 import com.pestphp.pest.coverage.PestCoverageProgramRunner
+import com.pestphp.pest.features.parallel.postprocessExecutionResult
 import com.pestphp.pest.statistics.PestUsagesCollector
 
 private val MUTATE_ARGUMENTS: MutableList<String> = mutableListOf("--mutate")
-private const val MUTATE_RELEASE_MAJOR_VERSION = 3
 
-class PestMutateProgramRunner : PestCoverageProgramRunner() {
+open class PestMutateProgramRunner : PestCoverageProgramRunner() {
     companion object {
         const val RUNNER_ID: String = "PestMutateRunner"
     }
 
     override fun doExecute(state: RunProfileState, environment: ExecutionEnvironment): RunContentDescriptor? {
         PestUsagesCollector.logMutationTestExecution(environment.project)
-        return super.doExecute(state, environment)
-    }
-
-    override fun canRun(executorId: String, profile: RunProfile): Boolean {
-        if (executorId != PestMutateTestExecutor.EXECUTOR_ID || profile !is PestRunConfiguration) {
-            return false
+        val contentDescriptor = super.doExecute(state, environment)
+        if (contentDescriptor != null) {
+            postprocessExecutionResult(contentDescriptor, environment, PestBundle.message("MUTATION_TESTING_IS_AVAILABLE_FROM_VERSION_3"))
         }
-
-        val project = profile.project
-        val interpreter = profile.interpreter
-        val config = PhpTestFrameworkSettingsManager.getInstance(project).getOrCreateByInterpreter(
-            PestFrameworkType.instance, interpreter, profile.getBaseFile(null, interpreter), true
-        ) ?: return false
-        val version = PhpTestFrameworkVersionCache.getCachedVersion(project, config)
-
-        return version == null || version.isOrGreaterThan(MUTATE_RELEASE_MAJOR_VERSION)
+        return contentDescriptor
     }
+
+    override fun canRun(executorId: String, profile: RunProfile): Boolean =
+        executorId == PestMutateTestExecutor.EXECUTOR_ID && profile is PestRunConfiguration
 
     override fun createCoverageArguments(targetCoverage: String?): MutableList<String> = MUTATE_ARGUMENTS
 
-    override fun getRunnerId(): String {
-        return RUNNER_ID
-    }
+    override fun getRunnerId(): String = RUNNER_ID
 }
--- src/main/kotlin/com/pestphp/pest/features/parallel/PestParallelProgramRunner.kt
+++ src/main/kotlin/com/pestphp/pest/features/parallel/PestParallelProgramRunner.kt
@@ -4,44 +4,38 @@ import com.intellij.execution.ExecutionException
 import com.intellij.execution.configurations.RunProfile
 import com.intellij.execution.configurations.RunProfileState
 import com.intellij.execution.configurations.RunnerSettings
+import com.intellij.execution.process.ProcessAdapter
+import com.intellij.execution.process.ProcessEvent
 import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.execution.runners.GenericProgramRunner
 import com.intellij.execution.runners.RunContentBuilder
+import com.intellij.execution.testframework.sm.runner.SMTestProxy.SMRootTestProxy
+import com.intellij.execution.testframework.sm.runner.ui.SMTRunnerConsoleView
 import com.intellij.execution.ui.RunContentDescriptor
+import com.intellij.notification.NotificationType
 import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.openapi.util.NlsSafe
 import com.jetbrains.php.PhpBundle
 import com.jetbrains.php.config.commandLine.PhpCommandSettings
 import com.jetbrains.php.config.commandLine.PhpCommandSettingsBuilder
+import com.jetbrains.php.phpunit.PhpUnitUtil
 import com.jetbrains.php.testFramework.PhpTestFrameworkSettingsManager
-import com.jetbrains.php.testFramework.PhpTestFrameworkVersionCache
 import com.pestphp.pest.PestBundle
 import com.pestphp.pest.PestFrameworkType
 import com.pestphp.pest.configuration.PestRerunProfile
 import com.pestphp.pest.configuration.PestRunConfiguration
+import com.pestphp.pest.configuration.PestVersionDetector
 import com.pestphp.pest.statistics.PestUsagesCollector
 
 private val PEST_PARALLEL_ARGUMENTS = mutableListOf("--parallel", "--log-teamcity", "php://stdout")
-private const val PEST_PARALLEL_SUPPORT_MAJOR_VERSION = 2
 
 class PestParallelProgramRunner : GenericProgramRunner<RunnerSettings>() {
     companion object {
         const val RUNNER_ID: String = "PestParallelRunner"
     }
 
-    override fun canRun(executorId: String, profile: RunProfile): Boolean {
-        if (executorId != PestParallelTestExecutor.EXECUTOR_ID || profile !is PestRunConfiguration) {
-            return false
-        }
-
-        val project = profile.project
-        val interpreter = profile.interpreter
-        val config = PhpTestFrameworkSettingsManager.getInstance(project).getOrCreateByInterpreter(
-            PestFrameworkType.instance, interpreter, profile.getBaseFile(null, interpreter), true
-        ) ?: return false
-        val version = PhpTestFrameworkVersionCache.getCachedVersion(project, config)
-
-        return version == null || version.isOrGreaterThan(PEST_PARALLEL_SUPPORT_MAJOR_VERSION)
-    }
+    override fun canRun(executorId: String, profile: RunProfile): Boolean =
+        executorId == PestParallelTestExecutor.EXECUTOR_ID && profile is PestRunConfiguration
 
     override fun doExecute(state: RunProfileState, environment: ExecutionEnvironment): RunContentDescriptor? {
         PestUsagesCollector.logParallelTestExecution(environment.project)
@@ -50,17 +44,17 @@ class PestParallelProgramRunner : GenericProgramRunner<RunnerSettings>() {
             state.execute(environment.executor, this)
         } else {
             val runConfiguration = environment.runProfile as? PestRunConfiguration
-            if (runConfiguration == null) throw ExecutionException(PestBundle.message("PEST_PARALLEL_IS_NOT_SUPPORTED_FOR_SELECTED_RUN_PROFILE"))
+                ?: throw ExecutionException(PestBundle.message("PEST_PARALLEL_IS_NOT_SUPPORTED_FOR_SELECTED_RUN_PROFILE"))
             val command = createPestParallelCommand(runConfiguration)
             runConfiguration.checkAndGetState(environment, command)?.execute(environment.executor, this)
         }
         if (executionResult == null) throw ExecutionException(PhpBundle.message("execution.result.is.null"))
-        return RunContentBuilder(executionResult, environment).showRunContent(environment.contentToReuse)
+        val contentDescriptor = RunContentBuilder(executionResult, environment).showRunContent(environment.contentToReuse)
+        postprocessExecutionResult(contentDescriptor, environment, PestBundle.message("PARALLEL_TESTING_IS_SUPPORTED_FROM_VERSION_2"))
+        return contentDescriptor
     }
 
-    override fun getRunnerId(): String {
-        return RUNNER_ID
-    }
+    override fun getRunnerId(): String = RUNNER_ID
 
     fun getArguments(): MutableList<String> = PEST_PARALLEL_ARGUMENTS
 }
@@ -74,4 +68,58 @@ internal fun createPestParallelCommand(runConfiguration: PestRunConfiguration): 
         PEST_PARALLEL_ARGUMENTS,
         false
     )
+}
+
+internal fun postprocessExecutionResult(
+    contentDescriptor: RunContentDescriptor,
+    environment: ExecutionEnvironment,
+    @NlsSafe versionRequirement: String,
+) {
+    val processHandler = contentDescriptor.processHandler
+    processHandler?.addProcessListener(object : ProcessAdapter() {
+        override fun processTerminated(event: ProcessEvent) {
+            val executionConsole = contentDescriptor.executionConsole as? SMTRunnerConsoleView ?: return
+            val rootProxy = executionConsole.resultsViewer.root as? SMRootTestProxy ?: return
+            if (rootProxy.isEmptySuite && !rootProxy.isTestsReporterAttached) {
+                handleEmptySuite(environment, versionRequirement)
+            }
+        }
+    })
+}
+
+private fun handleEmptySuite(
+    environment: ExecutionEnvironment,
+    @NlsSafe versionRequirement: String,
+) {
+    val profile = environment.runProfile as PestRunConfiguration
+    val project = profile.project
+    val interpreter = profile.interpreter ?: return
+    val config = PhpTestFrameworkSettingsManager.getInstance(project).getOrCreateByInterpreter(
+        PestFrameworkType.instance, interpreter, profile.getBaseFile(null, interpreter), true
+    ) ?: return
+
+    val version = if (!interpreter.isRemote) {
+        PestVersionDetector.instance.getVersion(project, interpreter, config.executablePath)
+    } else {
+        null
+    }
+
+    createAndShowNotification(environment, versionRequirement, version)
+}
+
+private fun createAndShowNotification(
+    environment: ExecutionEnvironment,
+    @NlsSafe versionRequirement: String,
+    version: String?,
+) {
+    PhpUnitUtil.NOTIFICATION_GROUP.createNotification(
+        versionRequirement,
+        NotificationType.ERROR
+    ).apply {
+        version?.let {
+            setTitle(PestBundle.message("CURRENT_PEST_VERSION_IS", it))
+        }
+        setSuggestionType(true)
+        notify(environment.project)
+    }
 }
