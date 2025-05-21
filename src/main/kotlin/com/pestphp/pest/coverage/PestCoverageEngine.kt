cbd786e4df584bed23db2ebd5aa9bddeea36c48b
Maksim Zuev <maksim.zuev@jetbrains.com>
2025-05-21 12:07:37 +0200

    [coverage] Proper deprecation of CoverageEngine#createCoverageSuite from run configuration
    GitOrigin-RevId: 2b32bfe999d40902eb4a2f7fb351f780e9a9d804

--- src/main/kotlin/com/pestphp/pest/coverage/PestCoverageEngine.kt
+++ src/main/kotlin/com/pestphp/pest/coverage/PestCoverageEngine.kt
@@ -5,6 +5,7 @@ import com.intellij.coverage.CoverageRunner
 import com.intellij.coverage.CoverageSuite
 import com.intellij.execution.configurations.RunConfigurationBase
 import com.intellij.execution.configurations.coverage.CoverageEnabledConfiguration
+import com.intellij.openapi.project.Project
 import com.jetbrains.php.phpunit.coverage.PhpCoverageSuite
 import com.jetbrains.php.phpunit.coverage.PhpUnitCoverageEngine
 import com.pestphp.pest.configuration.PestRunConfiguration
@@ -19,13 +20,15 @@ class PestCoverageEngine : PhpUnitCoverageEngine() {
     }
 
     override fun createCoverageSuite(
-        covRunner: CoverageRunner,
         name: String,
-        coverageDataFileProvider: CoverageFileProvider,
+        project: Project,
+        runner: CoverageRunner,
+        fileProvider: CoverageFileProvider,
+        timestamp: Long,
         config: CoverageEnabledConfiguration
     ): CoverageSuite? {
         if (config is PestCoverageEnabledConfiguration) {
-            return PhpCoverageSuite(name, config.getConfiguration().project, covRunner, coverageDataFileProvider, config.createTimestamp())
+            return PhpCoverageSuite(name, project, runner, fileProvider, timestamp)
         }
 
         return null
