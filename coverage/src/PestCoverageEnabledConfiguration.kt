c7343be062e9796903bd48bbaf62e1824c0495e3
Ilia Smirnov <ilia.smirnov@jetbrains.com>
2026-08-29 19:59:42 +0200

    WI-85867 Rename PhpUnitCoverageRunner to PhpCoverageRunner because it has no PHPUnit dependencies anymore
    GitOrigin-RevId: bd93b447eb62bf6a4379dc10fd39a6384d3200c2

--- coverage/src/PestCoverageEnabledConfiguration.kt
+++ coverage/src/PestCoverageEnabledConfiguration.kt
@@ -2,11 +2,11 @@ package com.intellij.pest.coverage
 
 import com.intellij.coverage.CoverageRunner
 import com.intellij.execution.configurations.coverage.CoverageEnabledConfiguration
-import com.intellij.php.coverage.PhpUnitCoverageRunner
+import com.intellij.php.coverage.PhpCoverageRunner
 import com.pestphp.pest.configuration.PestRunConfiguration
 
 class PestCoverageEnabledConfiguration(
     configuration: PestRunConfiguration
-) : CoverageEnabledConfiguration(configuration, CoverageRunner.getInstance(PhpUnitCoverageRunner::class.java)) {
+) : CoverageEnabledConfiguration(configuration, CoverageRunner.getInstance(PhpCoverageRunner::class.java)) {
     override fun coverageFileNameSeparator(): String = "@"
 }
